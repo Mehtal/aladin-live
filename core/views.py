817d2afc55b9edc4bb4ffce7b317473530965782
mehtal <mehtalkhalil@gmail.com>
2022-01-23 10:03:24 +0100

    niveau list view

--- core/views.py
+++ core/views.py
@@ -8,40 +8,31 @@ from .forms import GetBroadcastForm
 from django.contrib import messages
 from django.http import HttpResponse
 from django.http import Http404
+from librarry.models import Niveau
 
 
 def home(request):
-    form = GetBroadcastForm()
-    print("getgetgetgetgetgetgetgetgetget")
-    if request.method == 'POST':
-        form = GetBroadcastForm(request.POST or None)
-        if form.is_valid():
-            print("postpostpostpostpost")
-            code = form.cleaned_data.get('code', None)
-            print("--------------> code", code)
+    # form = GetBroadcastForm()
+    # if request.method == 'POST':
+    #     form = GetBroadcastForm(request.POST or None)
+    #     if form.is_valid():
+    #         code = form.cleaned_data.get('code', None)
+    #         print("--------------> code", code)
 
-            try:
-                print("--------------> try")
-                obj = get_object_or_404(Broadcast, code=code)
-                print("--------------> after tryyy")
+    #         try:
+    #             print("--------------> try")
+    #             obj = get_object_or_404(Broadcast, code=code)
+    #             print("--------------> after tryyy")
 
-                return redirect(obj.get_absolute_url())
-            except Http404:
-                print("--------------> inside exception")
-                messages.error(request, f"{code} n'est pas un code étudiant valide vérifiez votre sms ou contactez-nous pour un code valide")
-                return redirect("home")
+    #             return redirect(obj.get_absolute_url())
+    #         except Http404:
+    #             print("--------------> inside exception")
+    #             messages.error(
+    #                 request, f"{code} n'est pas un code étudiant valide vérifiez votre sms ou contactez-nous pour un code valide")
+    #             return redirect("home")
+    object_list = Niveau.objects.all()
 
-            # print("--------------> type obj", type(obj))
-            # if obj:
-            #     print("--------------> obj", obj)
-            #     print("--------------> ----e obj", type(obj))
-
-            #     return redirect(obj.get_absolute_url())
-            # else:
-            #     messages.error(request, 'Document deleted.')
-            #     return HttpResponse("/")
-
-    return render(request, "home.html", {"form": form})
+    return render(request, "home.html", {"object_list": object_list})
 
 
 class BroadcastDetail(DetailView):
